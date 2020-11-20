e5fe3f5134337ba9f3fc9e179a595508f281d314
ppbng <ppbasleng@gmail.com>
2020-11-21 00:02:14 +0700

    update enframe_data and bitString

--- FM_Flowcontrol/FM_Flowcontrol.ino
+++ FM_Flowcontrol/FM_Flowcontrol.ino
@@ -1,6 +1,20 @@
+#define sender 1  //config A or B
+#define receiver 0 //config A or B 
+String enFrame_data(int data,int angle){
+    String strout;
+    strout+= "1100"+String(sender)+String(receiver)+bitString(2,angle)+bitString(6,data)+"11";
+    return strout;
+}
+String bitString(int digit,int num){ //return String From num for selected digit
+  String strout;
+  strout = String(num,BIN);
+  while(strout.length()<digit){
+    strout = "0"+strout;
+  }
+  return strout; 
+}
 void setup() {
-  // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
