cf88e23a54be5d5e9969199200df448218c8f94c
ppbng <ppbasleng@gmail.com>
2020-11-23 17:43:49 +0700

    crc bugfix

--- FM_Flowcontrol/Frame.cpp
+++ FM_Flowcontrol/Frame.cpp
@@ -38,17 +38,17 @@ static String Frame::BINtoString(int digit, int num) { //return String of num fo
 }
 static String Frame::make_dataFrame(int data,int seq) {
   String strout;
-  strout += "1" + String(seq) + "00" + BINtoString(11, data) + "1"; //will implement crc later
+  strout += "1" + String(seq) + "00" + BINtoString(11, getcrc(data)) + "1"; //will implement crc later
   return strout;
 }
 static String Frame::make_ackFrame(int seq) {
   String strout;
-  strout += "1" + String(seq) + "01" + "00000000000" + "1";
+  strout += "1" + String(seq) + "01" + BINtoString(11, getcrc(0)) + "1";
   return strout;
 }
 static String Frame::make_UFrame(int code) { // 0 reset ->1 get -45->2 get 0->3 get 45
   String strout;
-  strout += "1010" + BINtoString(11, code) + "1"; //will implement crc later
+  strout += "1010" + BINtoString(11, getcrc(code)) + "1"; //will implement crc later
   return strout;
 }
 static String Frame::decodeFrame(String strin, String& ctrl, String& seq) {
